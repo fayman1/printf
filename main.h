6d9515a1cf2561969eb5066951c99d96e6bafa4c
Faith Muigai <fmuigai269@gmail.com>
2023-07-25 11:43:26 -0700

    main.h

--- main.h
+++ main.h
@@ -1,36 +1,40 @@
-#ifndef PRINT_F
-#define PRINT_F
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define INT_BITS (4 * 8)
 
-#include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <stdio.h>
 #include <limits.h>
 
 /**
- * struct convert - ....
- * @sym: .....
- * @f: ......
+ * struct call - struct call
+ * @t: t - flag for data type
+ * @f: function - assocated function
+ *
  */
-struct convert
+typedef struct call
 {
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
+	char t;
+	int (*f)(char *, va_list, int);
+} call_t;
 
 int _printf(const char *format, ...);
-int resolve(const char *format, conver_t f_list[], va_list arg_list);
-int put_char(char);
-int print_char(va_list);
-int print_str(va_list);
-int rot13(va_list);
-int print_int(va_list);
-int print_num(va_list);
-int un_int(va_list);
-int print_rev(va_list arg);
-char *rev_str(char *);
-int print_un_num(unsigned int);
-char *_memcpy(char *dest, char *str, unsigned int n);
+int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
+int print_buff(char *buff, unsigned int nbuff);
+int str_len(char *s);
+char *_strcpy(char *dest, char *src);
+int print_char(char *buff_dest, va_list arg, int buff_count);
+int print_str(char *buff_dest, va_list arg, int buff_count);
+int print_int(char *buff_dest, va_list list, int buff_count);
+int print_perc(char *buff_dest, va_list arg, int buff_count);
+int print_bin(char *buff_dest, va_list arg, int buff_count);
+int print_oct(char *buff_dest, va_list arg, int buff_count);
+int print_hex(char *buff_dest, va_list arg, int buff_count);
+int print_X(char *buff_dest, va_list arg, int buff_count);
+int print_uint(char *buff_dest, va_list arg, int buff_count);
+int print_rev(char *buff_dest, va_list arg, int buff_count);
+int print_R13(char *buff_dest, va_list arg, int buff_count);
 
 #endif
