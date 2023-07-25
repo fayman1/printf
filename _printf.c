12b3bfb5685ebb0ddc4d7049926f2a0cfd780f44
Faith Muigai <fmuigai269@gmail.com>
2023-07-25 02:42:06 -0700

    f

--- _printf.c
+++ _printf.c
@@ -2,17 +2,48 @@
 #include <stdarg.h>
 
 /**
+ * print_char - Print a single character to stdout
+ * @c: The character to print
+ * Return: Number of characters printed
+ */
+int print_char(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * print_string - Print a string to stdout
+ * @str: The string to print
+ * Return: Number of characters printed
+ */
+int print_string(char *str)
+{
+	if (str != NULL)
+	{
+		int len = 0;
+
+		while (str[len])
+			len++;
+		write(1, str, len);
+		return (len);
+	}
+	else
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+}
+
+/**
  * _printf - Printf function
  * @format: format.
  * Return: Number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
 	int p = 0;
 	va_list args;
-	char c;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
@@ -22,8 +53,7 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			p++;
+			p += print_char(*format);
 		}
 		else
 		{
@@ -32,31 +62,18 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					c = (char)va_arg(args, int);
-					write(1, &c, 1);
-					p++; break;
+					p += print_char((char)va_arg(args, int));
+					break;
 				case 's':
-					str = va_arg(args, char *);
-					  if (str != NULL)
-					  {
-						  int len = 0;
-
-						  while (str[len])
-							  len++;
-						  write(1, str, len);
-						  p += len;
-					  }
-					  else
-					  {
-						  write(1, "(null)", 6);
-						  p += 6;
-					  } break;
+					p += print_string(va_arg(args, char *));
+					break;
 				case '%':
-					  write(1, "%", 1);
-					  p++; break;
+					p += print_char('%');
+					break;
 				default:
-					  write(1, format - 1, 2);
-					 p += 2; break;
+					p += print_char(*(format - 1));
+					p += print_char(*format);
+					break;
 			}
 		}
 		format++;
