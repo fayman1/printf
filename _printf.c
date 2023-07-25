e2da7083349ebc3473743480686a4fe792ad0fdf
Faith Muigai <fmuigai269@gmail.com>
2023-07-25 02:35:49 -0700

    err

--- _printf.c
+++ _printf.c
@@ -9,64 +9,59 @@
 
 int _printf(const char *format, ...)
 {
-    int p = 0;
-    va_list args;
-    char c;
-    char *str;
+	int p = 0;
+	va_list args;
+	char c;
+	char *str;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			p++;
+		}
+		else
+		{
+			format++;
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            p++;
-        }
-        else
-        {
-            format++;
+			switch (*format)
+			{
+				case 'c':
+					c = (char)va_arg(args, int);
+					write(1, &c, 1);
+					p++; break;
+				case 's':
+					str = va_arg(args, char *);
+					  if (str != NULL)
+					  {
+						  int len = 0;
 
-            switch (*format)
-            {
-                case 'c':
-                    c = (char)va_arg(args, int);
-                    write(1, &c, 1);
-                    p++;
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str != NULL)
-                    {
-                        int len = 0;
-                        while (str[len])
-                            len++;
-                        write(1, str, len);
-                        p += len;
-                    }
-                    else
-                    {
-                        write(1, "(null)", 6);
-                        p += 6;
-                    }
-                    break;
-                case '%':
-                    write(1, "%", 1);
-                    p++;
-                    break;
-                default:
-                    write(1, format - 1, 2);
-                    p += 2;
-                    break;
-            }
-        }
-        format++;
-    }
-
-    va_end(args);
-    return (p);
+						  while (str[len])
+							  len++;
+						  write(1, str, len);
+						  p += len;
+					  }
+					  else
+					  {
+						  write(1, "(null)", 6);
+						  p += 6;
+					  } break;
+				case '%':
+					  write(1, "%", 1);
+					  p++; break;
+				default:
+					  write(1, format - 1, 2);
+					 p += 2; break;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (p);
 }
 
