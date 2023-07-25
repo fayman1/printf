808adb103427a930440b88a46964e39a32d7cfc8
Faith Muigai <fmuigai269@gmail.com>
2023-07-25 01:21:09 -0700

    error

--- _printf.c
+++ _printf.c
@@ -1,16 +1,15 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Number of characters printed
+ * Return: Number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-    int p = 0;
+    int printed_chars = 0;
     va_list args;
-    char c;
-    char *str;
 
     if (format == NULL)
         return (-1);
@@ -22,50 +21,56 @@ int _printf(const char *format, ...)
         if (*format != '%')
         {
             write(1, format, 1);
-            p++;
+            printed_chars++;
         }
         else
         {
-            format++;
+            format++; // Move past the '%'
 
+            // Check for the conversion specifiers
             switch (*format)
             {
                 case 'c':
-                    c = (char)va_arg(args, int);
-                    write(1, &c, 1);
-                    p++;
+                    {
+                        char c = (char)va_arg(args, int);
+                        write(1, &c, 1);
+                        printed_chars++;
+                    }
                     break;
                 case 's':
-                    str = va_arg(args, char *);
-                    if (str != NULL)
                     {
-                        int len = 0;
-
-                        while (str[len])
-                            len++;
-                        write(1, str, len);
-                        p += len;
-                    }
-                    else
-                    {
-                        write(1, "(null)", 6);
-                        p += 6;
+                        char *str = va_arg(args, char *);
+                        if (str != NULL)
+                        {
+                            int len = 0;
+                            while (str[len])
+                                len++;
+                            write(1, str, len);
+                            printed_chars += len;
+                        }
+                        else
+                        {
+                            write(1, "(null)", 6);
+                            printed_chars += 6;
+                        }
                     }
                     break;
                 case '%':
                     write(1, "%", 1);
-                    p++;
+                    printed_chars++;
                     break;
                 default:
                     write(1, format - 1, 2);
-                    p += 2;
+                    printed_chars += 2;
                     break;
             }
         }
+
         format++;
     }
 
     va_end(args);
-    return (p);
+
+    return (printed_chars);
 }
 
