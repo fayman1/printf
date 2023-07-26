3fc44b7f0a156386918aa97301f67ed306d39054
Faith Muigai <fmuigai269@gmail.com>
2023-07-26 14:57:49 -0700

    u

--- _printf.c
+++ _printf.c
@@ -108,22 +108,3 @@ int _printf(const char *format, ...)
     va_end(args);
     return (p);
 }
-
-int main(void)
-{
-    // Test cases
-    _printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", 39, 39);
-    _printf("Negative:[%d]\n", -762534);
-    _printf("Unsigned:[%u]\n", 2147484671);
-    _printf("Unsigned octal:[%o]\n", 20000001777);
-    _printf("Unsigned hexadecimal:[%x, %X]\n", 2147484671, 2147484671);
-    _printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    _printf("Address:[%p]\n", (void *)0x7ffe637541f0);
-    _printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", 12);
-    _printf("Unknown:[%r]\n");
-    return (0);
-}
-
