4947cd132e3b4a9d6330cba767de5533f54b28f8
Faith Muigai <fmuigai269@gmail.com>
2023-07-27 05:20:17 -0700

    err

--- print_char.c
+++ print_char.c
@@ -5,13 +5,13 @@
  * @buff_dest: buffer
  * @arg: list of arguments
  * @buff_count: index of buffer pointer
- * Return: buffer index
+ * Return: index
  */
 int print_char(char *buff_dest, va_list arg, int buff_count)
 {
-	char c = va_arg(arg, int);
+	char ch = va_arg(arg, int);
 
-	buff_dest[buff_count] = c;
+	buff_dest[buff_count] = ch;
 
 	return (++buff_count);
 }
